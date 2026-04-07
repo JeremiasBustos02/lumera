fb0db5288f995e4d569d84f61f8563fa1ea5a5f4
jeremiasbustos02 <jeremias.e.bustos@gmail.com>
2026-04-07 19:16:08 -0300

    chore(root): initialize project structure and base microservices

---
+++ backend/clinic-core-service/src/main/java/com/lumera/clinic_core_service/ClinicCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lumera.clinic_core_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicCoreServiceApplication.class, args);
+	}
+
+}
---
+++ backend/clinic-core-service/src/test/java/com/lumera/clinic_core_service/ClinicCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumera.clinic_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
